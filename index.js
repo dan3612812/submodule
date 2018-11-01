df9f5aa49579b95abc781c204a2db6790018a1b2
BYS <dan3612812@gmail.com>
2018-11-01 16:27:36 +0800

    add getRandom fun

---
+++ index.js
@@ -0,0 +1,7 @@
+exports.getRandom = function (len) {
+    let text = "";
+    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (let i = 0; i < len; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text
+}
